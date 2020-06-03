04f983bbe36726224fba8afbfe1a86a0c8490504
josiah <josiahdamiwilliams@gmail.com>
2020-06-03 16:01:23 +0100

    Day 01

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -37,15 +38,56 @@ room['treasure'].s_to = room['narrow']
 # Main
 #
 
-# Make a new player object that is currently in the 'outside' room.
-
+# Make a new new_player object that is currently in the 'outside' room.
+new_player = Player('joe', room['outside'])
 # Write a loop that:
 #
 # * Prints the current room name
+print(f"Current Location:{new_player.current_room.name}")
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
 #
+command = ''
+while command != 'q':
+    command = input("Choose a direction to move . Enter n, s, e, or w:")
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+#CODE
+directions = {'n': 'n_to', 's': 's_to', 'e': 'e_to', 'w': 'w_to'}
+
+while true:
+	  print(str(new_player.current_room))
+      choice = input("Which way, Gandalf? ")
+
+	direction = directions[choice]
+
+	try:
+		# if command == 'n':
+		# 	if new_player.current_room.n_to:
+		# 		new_player.current_room = new_player.current_room.n_to
+		# 	else:
+		# 		print("Sorry, there isn't a room to the North. Please choose another direction.")
+		# elif command == 's':
+		# 	if new_player.current_room.s_to:
+		# 		new_player.current_room = new_player.current_room.s_to
+		# 	else:
+		# 		print("Sorry, there isn't a room to the South. Please choose another direction.")
+		# elif command == 'e':
+		# 	if new_player.current_room.e_to:
+		# 		new_player.current_room = new_player.current_room.e_to
+		# 	else:
+		# 		print("Sorry, there isn't a room to the East. Please choose another direction.")
+		# elif command == 'w':
+		# 	if new_player.current_room.w_to:
+		# 		new_player.current_room = new_player.current_room.w_to
+		# 	else:
+		# 		print("Sorry, there isn't a room to the West. Please choose another direction.")
+		# elif command == 'q':
+		# 	exit(0)
+	player.current_room = getattr(player.current_room, direction)
+    
+    except AttributeError:
+		print("Sorry, no room in that direction. Please choose another direction.")
--- src/room.py
+++ src/room.py
@@ -1,2 +1,12 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    """ A class to hold room information.""" 
+    def __init__(self, name, desc, n_to, e_to, s_to, w_to ):
+        self.name = name
+        self.desc = desc
+        self.n_to = n_to
+        self.e_to = e_to
+        self.s_to = s_to
+        self.w_to = w_to
+	
